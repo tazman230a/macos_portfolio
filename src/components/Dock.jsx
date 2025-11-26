d61d76b0bcd0d2d4580e7f71ec92d3af9f3eb4d7
tazman230a <76573669+tazman230a@users.noreply.github.com>
2025-11-26 15:15:11 -0800

    implement a hoc for managing window state and add first window - terminal

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,10 +1,12 @@
 import { dockApps } from "#constants"
+import useWindowStore from "#store/window";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useRef } from "react"
 import { Tooltip } from 'react-tooltip'
 
 const Dock = () => {
+    const { openWindow, closeWindow, windows} = useWindowStore();
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -59,8 +61,21 @@ const Dock = () => {
 
 
     const toggleApp = (app) => {
-        // TODO Implement Open Window Logic
-    }
+        if(!app.canOpen) return;
+
+        const window = windows[app.id];
+
+        if(!window) {
+            console.error(`Window not found for app: ${app.id}`);
+            return;
+        }
+
+        if(window.isOpen) {
+            closeWindow(app.id);
+        } else {
+            openWindow(app.id);
+        }
+    };
 
   return (
     <section id="dock">
